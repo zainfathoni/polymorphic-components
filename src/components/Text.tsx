f7025b54316d40c8df6748f1442cff5bdc1571c9
Zain Fathoni <me@zainf.dev>
2022-03-20 01:32:22 +0800

    docs: why the simple polymorphic component is bad

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,5 +4,13 @@ export const Text = ({ as, children }: any) => {
 }
 
 /**
- * <Text>Hello, world!</Text>
+ * <Text as="div">Hello, world!</Text>
+ *
+ * as can receive invalid element
+ * No attributes support
+ * Wrong attributes can be passed for a valid element
+ *
+ * Why is this bad?
+ * - Terrible DX
+ * - Bugs can (and will) creep in
  */
