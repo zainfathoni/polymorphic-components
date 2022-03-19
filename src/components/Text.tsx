d6b38077f1dfa7ef66db432ea2dbf921fd813280
Zain Fathoni <me@zainf.dev>
2022-03-20 01:50:52 +0800

    docs: document the current state

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,19 +5,18 @@ type TextProps<T> = {
   as?: T
 }
 
-export const Text = <T extends React.ElementType>({ as, children }: TextProps<T>) => {
-  const Component = as || 'span'
-  return <Component>{children}</Component>
-}
-
 /**
  * <Text as="div">Hello, world!</Text>
  *
- * as can receive invalid element
- * No attributes support
- * Wrong attributes can be passed for a valid element
+ * - ~as can receive invalid element~
+ * - No attributes support
+ * - Wrong attributes can be passed for a valid element
  *
  * Why is this bad?
  * - Terrible DX
  * - Bugs can (and will) creep in
  */
+export const Text = <T extends React.ElementType>({ as, children }: TextProps<T>) => {
+  const Component = as || 'span'
+  return <Component>{children}</Component>
+}
